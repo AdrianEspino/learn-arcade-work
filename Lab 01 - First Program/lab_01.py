2c9ecd585273c892795ce9066987b02d999f5109
AdrianEspino <adrian.espino@edu.uah.es>
2021-02-18 20:15:28 +0100

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,6 @@
-Hi
+print("Hi, my name is Adrián")
+print("I'm studying at the university")
+print("Today I'm learning how to use Git")
+print("Later, we'll have to create our own videogame")
+print("I told my friends:\"This is amazing\"")
+
